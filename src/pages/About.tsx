9bcb211a267fd246e3e54b8b8c9a2c39a06a3734
Joe Tower <joe@fourkitchens.com>
2025-04-18 12:55:27 -0500

    Add about page and pages, but unlinked

---
+++ src/pages/About.tsx
@@ -0,0 +1,16 @@
+import MainLayout from '../components/layouts/MainLayout';
+import Paragraph from '../components/text/Paragraph';
+import Heading from '../components/text/Heading';
+
+function About() {
+  return (
+    <MainLayout baseClass='about-page'>
+      <Heading level='h1' baseClass='intro__heading' content='About the app' />
+      <Paragraph baseClass='intro__paragraph' style='emphasized'>
+        <p>This is an example site to explore the awesomeness of <strong>React</strong> and component libraries.</p>
+      </Paragraph>
+    </MainLayout>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import MainLayout from '../components/layouts/MainLayout';
+import Paragraph from '../components/text/Paragraph';
+import Heading from '../components/text/Heading';
+
+function Home() {
+  return (
+    <MainLayout baseClass='home-page'>
+      <Heading level='h1' baseClass='intro__heading' content='Hello Vite + React!' />
+      <Paragraph baseClass='intro__paragraph' style='emphasized'>
+        <p>This is an example site to explore the awesomeness of <strong>React</strong> and component libraries.</p>
+      </Paragraph>
+      <Paragraph baseClass='paragraph' style='default' >
+        <p>This app applies the components from the <a href="/storybook" title="Storybook Components">Component Library</a></p>
+      </Paragraph>
+    </MainLayout>
+  )
+}
+
+export default Home;
