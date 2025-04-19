ebd154b8fabf05ea13ef514af9c8e6820b987dba
Joe Tower <joetower@gmail.com>
2025-04-19 10:44:12 -0500

    refine width of text width custom property

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,8 @@ function About() {
     <MainLayout baseClass='page about-page'>
       <Heading level='h1' baseClass='intro__heading' content='About the app' />
       <Paragraph baseClass='intro__paragraph' style='emphasized' width='content'>
-        <p>This is an example site to explore the awesomeness of <strong>React</strong> and component libraries.</p>
+        <p>This is an example site to explore the awesomeness of <strong>React</strong> and component libraries. 
+        This site is a work in progress and will get updated many times per week.</p>
       </Paragraph>
     </MainLayout>
   );
