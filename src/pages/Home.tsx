7f4696971e8fdad8d2fba3d26b94a00fe7601180
Joe Tower <joe@fourkitchens.com>
2025-04-28 11:11:42 -0500

    Add more components to main app index

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import MainLayout from '../components/layouts/MainLayout';
 import Paragraph from '../components/text/TextBlock';
 import Heading from '../components/text/Heading';
 import ResponsiveImage from '../components/responsive-image/ResponsiveImage';
+import Quote from '../components/text/Blockquote';
+import Callout from '../components/callout/Callout';
 
 function Home() {
   return (
@@ -16,6 +18,8 @@ function Home() {
       <Paragraph baseClass='paragraph' style='default' width='content'>
         <p>This app applies the components from the <a href="/storybook" title="Storybook Components (opens in new tab/window)" target=''>Component Library &#8599;</a>. Here are some elephants.</p>
       </Paragraph>
+      <Quote align="left" baseClass='quote' style='quote' content="Never worry about failure. It's gonna happen. But that's OK: Go ahead, take chances… and you might just find that what feels like failure isn't failure at all, it's what inspires you to dream even bigger." author="Kermit the Frog" theme='tertiary' width='content' />
+      <Callout heading='Callout component' alignment='center' animation={true} text="This is a callout component. It can be used to highlight important information." theme='secondary' link='#' linkText='Call to Action' linkTitle='Link going nowhere for demonstration' />
       <ResponsiveImage 
       imageName="2-12-6-32 AM-2023-FUJIFILM-X-T3-DSCF1862" 
       alt="Elephants drinking water | Uganda" 
