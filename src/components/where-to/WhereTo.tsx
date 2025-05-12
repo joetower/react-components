8603074d73554dcb1ed6a2eeb460245513ffa1b9
Joe Tower <joe@fourkitchens.com>
2025-05-12 16:21:26 -0500

    Update where to with wavy bg animation

--- src/components/where-to/WhereTo.tsx
+++ src/components/where-to/WhereTo.tsx
@@ -56,7 +56,7 @@ export default function Places({ whereToContent = "let's go somewhere", whereToH
   }
 
   return (
-    <div className='places' data-component-width={width} data-component-theme={theme}> 
+    <div className='places' data-component-width={width} data-component-theme={theme}>
       <div className={`places__inner ${baseClass || ''}`} >
         <Heading content={whereToHeading} level='h2' baseClass={`places__heading`} />
         <TextBlock baseClass={`places__content`} style='default' width={'full'}>
@@ -67,6 +67,7 @@ export default function Places({ whereToContent = "let's go somewhere", whereToH
             Enter a state or country name:
           </label>
           <input
+            id={`item-${items[0].id}`}
             type="text"
             className='places__input-field'
             value={items[0].title}
@@ -92,6 +93,18 @@ export default function Places({ whereToContent = "let's go somewhere", whereToH
         )}
         {children}
       </div>
+      <svg className="places__bg" xmlns="http://www.w3.org/2000/svg" version="1.1" width="1440" height="560" preserveAspectRatio="none" viewBox="0 0 1440 560">
+        <g mask="url(&quot;#places-mask&quot;)" fill="none">
+          <rect width="1440" height="560" x="0" y="0" fill='var(--color--layer-1)'></rect>
+          <path d="M 0,37 C 72,79.4 216,235.8 360,249 C 504,262.2 576,103 720,103 C 864,103 936,244.6 1080,249 C 1224,253.4 1368,149.8 1440,125L1440 560L0 560z" fill='var(--color--layer-2)'></path>
+          <path d="M 0,356 C 144,384.4 432,513 720,498 C 1008,483 1296,324.4 1440,281L1440 560L0 560z" fill='var(--color--layer-3)'></path>
+        </g>
+        <defs>
+          <mask id="places-mask">
+            <rect width="1440" height="560" fill="#ffffff"></rect>
+          </mask>
+        </defs>
+      </svg>
     </div>
   );
 }
